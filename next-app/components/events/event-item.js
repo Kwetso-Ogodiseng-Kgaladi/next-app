08c156abdf5cb3317e5e414df2f5ca336418dba0
kwetso12 <kgaladikwetso@gmail.com>
2023-09-06 14:18:54 +0200

    Event handling

--- next-app/components/events/event-item.js
+++ next-app/components/events/event-item.js
@@ -1,33 +1,38 @@
 import Link from 'next/link';
 
+import classes from './event-item.module.css';
+import Button from '../ui/button';
+
 function EventItem(props) {
-const { title, image, date, location, id} = props;
+  const { title, image, date, location, id } = props;
 
-const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
-  day:'numeric',
-  month:'long',
-  year: numeric,  
-})
-const formattedAddress = location.replace(',','\n');
-const exploreLink = `/events/${id}`;
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  })
+  const formattedAddress = location.replace(',', '\n');
+  const exploreLink = `/events/${id}`;
 
-  return <li>
-<img src={'/' + image} alt="title" />
-<div>
-    <div>
-        <h2>{title}</h2>
-        <div>
+  return (
+    <li className={classes.item}>
+      <img src={'/' + image} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
             <time>{humanReadableDate}</time>
-        </div>
-        <div>
+          </div>
+          <div className={classes.address}>
             <address>{formattedAddress}</address>
+          </div>
         </div>
-    </div>
-    <div>
-        <link href={exploreLink}>Explore Event</link>
-    </div>
-</div>
-  </li>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>Explore Event</Button>
+        </div>
+      </div>
+    </li>
+  )
 }
 
 export default EventItem;
