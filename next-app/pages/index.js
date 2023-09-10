4d9e0eb14dcb5b062bd5dadde78fc6914aebf684
kwetso12 <kgaladikwetso@gmail.com>
2023-09-10 21:27:03 +0200

    added a function of all events

--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -1,11 +1,12 @@
 import { getFeaturedEvents } from "@/dummy-data";
 import EventList from "../components/events/event-list";
-
+//import EventsSearch from "@/components/events/events-search";
 function HomePage () {
 const featuredEvents = getFeaturedEvents();
 
   return (
     <div>
+      
      <EventList items={featuredEvents} />
     </div>
   );
