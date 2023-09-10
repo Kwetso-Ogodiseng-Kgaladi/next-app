d7950fba93193be92ebbae7a9527062bda920ac8
kwetso12 <kgaladikwetso@gmail.com>
2023-09-10 21:26:16 +0200

    updated

--- next-app/components/ui/button.js
+++ next-app/components/ui/button.js
@@ -3,11 +3,21 @@ import classes from './button.module.css';
 
 
 function Button(props) {
+
+  if (props.link) {
+    return (
+     ( <Link href={props.link} 
+      className={classes.btn}>
+        {props.children} 
+          </Link> )
+      );
+  }
 return (
-<Link href={props.link}>
-  <a className={classes.btn}></a>{props.children} 
-    </Link>
+  <button className={classes.btn} 
+  onClick={props.onClick}>
+    {props.children}</button>
 )
+
 };
 
 export default Button;
