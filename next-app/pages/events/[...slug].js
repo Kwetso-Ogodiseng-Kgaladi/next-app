224e1796bf8d19f09ca6e0a50dcab7a74c971b06
kwetso12 <kgaladikwetso@gmail.com>
2023-09-14 15:22:52 +0200

    updated

--- next-app/pages/events/[...slug].js
+++ next-app/pages/events/[...slug].js
@@ -1,79 +1,63 @@
 import Button from "@/components/ui/button";
 import { getFilteredEvents } from "@/dummy-data";
-//import Button from "@/components/ui/button";
 import { useRouter } from "next/router";
 import { Fragment } from "react";
-
-//import ResultsTitle from 'next/router';
+import ErrorAlert from "@/components/ui/error-alert";
 import EventList from "@/components/events/event-list";
-//import Button from "@/components/ui/button";
+import ResultsTitle from "@/components/events/results-title";
 
-function FilteredEventsPage () {
-const router = useRouter();
-const filteredData = router.query.slug;
 
-if (!filteredData) {
-  return <p className='center'>Loading...</p>;
-}
-
-const filteredYear = filteredData[0];
-const filteredMonth = filteredData[1];
-
-const numYear = +filteredYear;
-const numMonth = +filteredMonth;
-
-if(isNaN(numYear) || 
-isNaN(numMonth) || 
-numYear < 2020 ||
-//  numYear < 2021 || 
-numMonth < 1 || 
-numMonth > 12
-) {
-  return (
-  <Fragment>
-    <p>Invalid filter. Please adjust your values!</p>
-<div className='center'>
-      <Button link='/events'>Show All Events</Button>
-      </div>
-
-  </Fragment>
-  );
-  
-}
-
-const filteredEvents = getFilteredEvents( {
-  year: numYear,
-  month: numMonth,
-});
-
-if (!filteredEvents || filteredEvents.length ===0) {
+export default function FilteredEventsPage() {
+  const router = useRouter();
+  const filterData = router.query.slug;
+  if (!filterData) {
+    return <p className='center'>Loading...</p>
+  }
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values!</p>
+        </ErrorAlert>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+        <br/>
+      </Fragment>
+    )
+  }
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+        <br/>
+      </Fragment>
+    )
+  }
+  const date = new Date(numYear, numMonth - 1);
   return (
     <Fragment>
-      <p>No events found for the chosen filter!</p>
-      <div className='center'>
-      <Button link='/events'>Show All Events</Button>
-      </div>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
     </Fragment>
   );
 }
-
-  const date = new Date(numYear, numMonth - 1);
-
-    return (
-      <Fragment> 
-      
-         {/* <ResultsTitle date={date} /> */}
-         <h3>Events in {new Date(date).toLocaleDateString('en-gb',{year:'2-digit',month:'short'})}</h3>
-        <EventList items={filteredEvents }/>
-
-       <div>
-         <h1>Filtered Events</h1>
-      </div>
-      
-       </Fragment>
-       
-    );
-    
-  }
-  
-  export default FilteredEventsPage;
---
+++ next-firebase/my-app/pages/events/index.js
@@ -0,0 +1,39 @@
+import { getAllEvents } from "@/helpers/api-util";
+import EventList from "@/components/events/event-list";
+import EventsSearch from "@/components/events/events-search";
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+function AllEventsPage (props) {
+
+  
+  const router = useRouter();
+  // const { events } = props;
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}/`;
+
+    router.push(fullPath);
+
+  }
+  return (
+      <Fragment> 
+        <EventsSearch onSearch={findEventsHandler}/>
+       <EventList items={props.events}/>
+      </Fragment>
+    );
+  }
+
+  export async function getStaticProps() {
+    const events = await getAllEvents();
+
+    return {
+      props: {
+        events: events
+      },
+      revalidate: 60
+    }
+
+  }
+  
+  export default AllEventsPage;
